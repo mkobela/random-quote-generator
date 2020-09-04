5d1461fa44c052dd092f27b15c4f9c3fd18dfee0
Michael Kobela <mkobela@gmail.com>
2020-09-04 15:02:18 -0400

    Added extra credit features

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# random-quote-generator
- A random quote generator
+## Project Title
+Random quote generator
+
+## Features
+The random quote generator will automatically generate a new quote every 10 seconds. Or a user can generate a new quote
+on demand by pressing the "Show another quote" button.
+
+## Github location
+https://github.com/mkobela/random-quote-generator
--- js/script.js
+++ js/script.js
@@ -14,67 +14,78 @@ Project 1 - A Random Quote Generator
  * @property {string} source - person or character who said quote.
  * @property {string} citation - reference to source of quote
  * @property {number} year - year the quote originated
+ * @property {string} tags - additional properties that add context
 ***/
 let quotes = [
   {
     quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier.',
     source: 'Mother Teresa',
     citation: '',
-    year: 0
+    year: null,
+    tags: []
   },
   {
     quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
     source: 'Franklin D. Roosevelt',
     citation: '',
-    year: 0
+    year: null,
+    tags: []
   },
   {
     quote: 'Don\'t judge each day by the harvest you reap but by the seeds that you plan',
     source: 'Robert Louis Stevenson',
     citation: '',
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: 'The future belongs to those who believe in the beauty of their dreams.',
     source: 'Eleanor Roosevelt',
     citation: '',
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
     source: 'Benjamin Franklin',
     citation: '',
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: 'And so we beat on, boats against the current, borne back ceaselessly into the past.',
     source: 'F. Scott Fitzgerald',
     citation: 'The Great Gatsby',
-    year: 1925
+    year: 1925,
+    tags: []
   },
   {
     quote: 'It is during our darkest moments that we must focus to see the light.',
     source: 'Aristotle',
     citation: '',
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: 'Whoever is happy will make others happy too.',
     source: 'Anne Frank',
     citation: '',
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: 'Live in the sunshine, swim the sea, drink the wild air',
     source: 'Ralph Waldo Emerson',
     citation: 'The Conduct of Life',
-    year: 1860
+    year: 1860,
+    tags: ['accomplishment', 'conformity', 'individuality']
   },
   {
     quote: 'It\'s the possibility of having a dream come true that makes life interesting.',
     source: 'Paulo Coelho',
     citation: 'The Alchemist',
-    year: 2014
+    year: 2014,
+    tags: []
   }
 ];
 
@@ -89,6 +100,18 @@ function getRandomQuote(limit) {
 
   // retrun random quote from quotes array
   return (quotes[randomNumber]);
+}
+
+/***
+ * code adapted from https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+ * @function getRandomColor
+ * @returns {string} color - a random color
+***/
+function getRandomColor(){
+  let color;
+  color = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+  return color;
 }
 
 /***
@@ -114,6 +137,11 @@ function printQuote() {
     quoteHTML += `<span class="year">${quote.year}</span>`;
   }
 
+  // check quote for tags
+  if (quote.tags.length > 0) {
+    quoteHTML += `<span class="tags">${quote.tags.join(', ')}</span>`;
+  }
+
   // finialize quote HTML
   quoteHTML += `</p>`;
 
@@ -121,7 +149,13 @@ function printQuote() {
   // Note: in the instructions item 8.6 it states to return the HTML
   // but after discussing with "@Lee V" decided this was not necessary.
   document.getElementById('quote-box').innerHTML = quoteHTML;
+
+  // generate a random backgroud color for quote
+  document.getElementById('quote-box').parentElement.style.backgroundColor = getRandomColor();
 }
+
+// create timer to auto generate quotes every 10 seconds
+setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
